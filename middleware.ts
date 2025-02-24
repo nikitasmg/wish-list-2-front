e70b5eb260e1dbdae90c240a15a8952b1f21ba80
nvsmagin <nvsmagin@gid.ru>
2025-02-24 15:18:35 +0500

    feat: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')?.value;
-  console.log('Token:', token); // Логируем токен
-
-  if (!token) {
-    console.log('Redirecting to /login'); // Логируем редирект
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-
-  return NextResponse.next();
-}
-
-export const config = {
-  // matcher: ['/wishlist/:path*'],
-};
+// import { NextRequest, NextResponse } from 'next/server';
+//
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get('token')?.value;
+//   console.log('Token:', token); // Логируем токен
+//
+//   if (!token) {
+//     console.log('Redirecting to /login'); // Логируем редирект
+//     return NextResponse.redirect(new URL('/login', req.url));
+//   }
+//
+//   return NextResponse.next();
+// }
+//
+// export const config = {
+//   // matcher: ['/wishlist/:path*'],
+// };
