c4abaaad3d67039d2c88c90815709df277dcc6cc
nvsmagin <nvsmagin@gid.ru>
2025-02-24 23:36:17 +0500

    feat: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -7,7 +6,9 @@ export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/wishlist')) {
     // Получаем куку 'token'
     const token = request.cookies.get('token')?.value;
-    console.log(request.cookies.get('token'))
+    console.log('Token:', token); // Логируем значение куки
+    console.log('All cookies:', request.cookies.getAll()); // Логируем все куки
+
     // Если куки нет, выполняем редирект на /login
     if (!token) {
       const loginUrl = new URL('/login', request.url);
@@ -19,7 +20,6 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// Конфигурация middleware
 export const config = {
   matcher: '/wishlist/:path*', // Применяем middleware только к путям, начинающимся с /wishlist
 };
