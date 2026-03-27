639a9cd851b212deb6550c53c7c49aed3a2613af
nvsmagin <nvsmagin@gid.ru>
2026-03-27 19:29:57 +0500

    fix: Метрика яндекса

--- components/metrica.tsx
+++ components/metrica.tsx
@@ -17,8 +17,6 @@ export function Metrika() {
   return (
     <Script id="yandex-metrika">
       {`
-        <!-- Yandex.Metrika counter -->
-<script type="text/javascript">
     (function(m,e,t,r,i,k,a){
         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}
         m[i].l=1*new Date()
@@ -26,11 +24,7 @@ export function Metrika() {
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
     })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108083039', 'ym')
 
-    ym(108083039, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:'dataLayer', referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true})
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/108083039" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter --> 
-      `}
+    ym(108083039, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:'dataLayer', referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true})`}
     </Script>
   );
 }
